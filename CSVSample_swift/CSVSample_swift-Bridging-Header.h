eadfc11b116467e8206473445af2b302609bc348
Kentarou <kanno@kentarou.me>
2016-07-20 07:45:02 +0900

    add Bridging-Header

--- CSVSample_swift/CSVSample_swift-Bridging-Header.h
+++ CSVSample_swift/CSVSample_swift-Bridging-Header.h
@@ -6,8 +6,4 @@
 //  Copyright © 2016年 KentarOu. All rights reserved.
 //
 
-#ifndef CSVSample_swift_Bridging_Header_h
-#define CSVSample_swift_Bridging_Header_h
-
-
-#endif /* CSVSample_swift_Bridging_Header_h */
+#import <SDWebImage/UIImageView+WebCache.h>
