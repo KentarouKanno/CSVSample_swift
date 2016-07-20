112182361cd1ebaf6f6738606f41b6d0fb980d02
Kentarou <kanno@kentarou.me>
2016-07-20 09:08:21 +0900

    add APIManager

--- CSVSample_swift/APIManager.swift
+++ CSVSample_swift/APIManager.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+class APIManager {
+    
+    class func  downloadCSVData(task: (success: Bool) -> () ) {
+        
+        // CSVダウンロード
+        let url = NSURL(string: "http://www.kentar0u.sakura.ne.jp/data.csv")
+        let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration(), delegate: nil, delegateQueue: NSOperationQueue.mainQueue())
+        let req = NSURLRequest(URL: url!)
+        
+        let task = session.dataTaskWithRequest(req, completionHandler: { (data, response, error) in
+            
+            if let data = data {
+                
+                // ダウンロード成功時
+                if CSVManager().csvDataWriteToFile(data) {
+                    // CSVデータファイル保存成功
+                    task(success: true)
+                } else {
+                    task(success: false)
+                }
+            
+            } else {
+                // エラー処理
+                print("Error!")
+                task(success: false)
+            }
+        })
+        task.resume()
+    }
+}
