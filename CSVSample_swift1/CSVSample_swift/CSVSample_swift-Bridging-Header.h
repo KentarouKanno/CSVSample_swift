d2627cc0bb6cd6add1967027d238079ffd74eba4
Kentarou <kanno@kentarou.me>
2016-08-27 13:58:17 +0900

    Delete

--- CSVSample_swift1/CSVSample_swift/CSVSample_swift-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  CSVSample_swift-Bridging-Header.h
-//  CSVSample_swift
-//
-//  Created by Kentarou on 2016/07/19.
-//  Copyright © 2016年 KentarOu. All rights reserved.
-//
-
-#import <SDWebImage/UIImageView+WebCache.h>
--- CSVSample_swift1/Pods/Target Support Files/Pods-CSVSample_swift/Pods-CSVSample_swift-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_CSVSample_swiftVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_CSVSample_swiftVersionString[];
-
